2efc6456f9b9f3eb03e6534144178c63dd0e6253
JavAlisson <javalisson@gmail.com>
2017-04-25 11:51:34 -0300

    Refatorada a classe ContaCorrente para utilizar a classe Operacao. Implementada a operacao de transferência entre contas. Adicionados titular e número de conta.

--- JavaBanco/src/javabanco/entidade/ContaCorrente.java
+++ JavaBanco/src/javabanco/entidade/ContaCorrente.java
@@ -1,30 +1,54 @@
 package javabanco.entidade;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ContaCorrente {
+	private String titular;
+	private int numero;
+
 	private float saldo;
-	private ArrayList<Float> operacoes = new ArrayList<Float>();
-	
+	private ArrayList<Operacao> operacoes = new ArrayList<Operacao>();
+
 	public float getSaldo() {
 		return saldo;
 	}
-	
+
 	public float debito(float valor) {
 		saldo -= valor;
-		operacoes.add(-valor);
+		Operacao op = new Operacao(valor, "DEBITO", new Date());
+		operacoes.add(op);
 		return saldo;
 	}
-	
+
 	public float credito(float valor) {
 		saldo += valor;
-//		Operacao o = new Operacao(valor, "CREDITO", new Date());
-		operacoes.add(valor);
+		Operacao op = new Operacao(valor, "CREDITO", new Date());
+		operacoes.add(op);
 		return saldo;
 	}
-	
-	public ArrayList<Float> extrato() {
+
+	public ArrayList<Operacao> extrato() {
 		return operacoes;
 	}
-	
+
+	public float transferencia(float valor, ContaCorrente ccDestino) {
+		this.debito(valor);
+		ccDestino.credito(valor);
+		return saldo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public ContaCorrente(int numero, String titular) {
+		super();
+		this.numero = numero;
+		this.titular = titular;
+	}
 }
